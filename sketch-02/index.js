b92d7278a786ee9ac5a3eacf7c8cb53a8b4b910f
itskaustubh <kaustubhpb@gmail.com>
2022-03-31 18:11:31 +0000

    Deploying to gh-pages from @ itskaustubh/canvas-sketches@c8130e9e16009c103a8336b2836bbdde264f3e10 🚀

--- sketch-02/index.js
+++ sketch-02/index.js
@@ -2160,70 +2160,41 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+const degToRad = (deg) => deg / 180 * Math.PI
+
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  return ({ context : ctx, width, height }) => {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, width, height);
 
-    let offList = [
-  false,
-  true,
-  false,
-  true,
-  false,
-  false,
-  true,
-  true,
-  false,
-  true,
-  false,
-  false,
-  true,
-  false,
-  true,
-  true,
-  false,
-  true,
-  false,
-  true,
-  true,
-  true,
-  false,
-  true,
-  true
-]
+    ctx.fillStyle = 'black'
+    x = width * 0.5
+    y = height * 0.5
+    w = width * 0.01
+    h = height * 0.1
 
-    context.lineWidth = 5
-    context.strokeStyle = 'black'
+    const count = 12
+    const slice = 360 / 12
 
-    const w = width * 0.10, h = height * 0.10 
-    const gap = width * 0.03
-    const ix = (width / 2) - 
-                ( ((w * 5) + (gap * 4)) / 2)
-    const iy = (height / 2) - 
-                ( ((h * 5) + (gap * 4)) / 2)
-    let x,y
-    const off = width * 0.02
+    
+    
+    for(let i=0;i<count;i++){
+      x = width * Math.random()
+      y = height * Math.random()
+      w = width * Math.random() * (0.04 - 0.01) + 0.01
+      h = height * Math.random()* (0.4 - 0.1) + 0.1
+      for(let j=0;j<count;j++){
+        ctx.save()
+        ctx.beginPath()
+        ctx.translate(x,y)
+        ctx.rotate(degToRad(slice * j))
+        ctx.rect(-(w * 0.5),-(h * 0.5),w,h)
+        ctx.fill()
+        ctx.restore()
+      }
+    }  
 
 
-    for(let i=0;i<5;i++){
-        for(let j=0;j<5;j++){
-            context.beginPath()
-            x = ix + ((w + gap) * i)
-            y = iy + ((h + gap) * j)
-            context.strokeStyle = '#36454F'
-            context.rect(x, y, w,h)
-            context.stroke()
-            if(!offList[(5 * i) + j]){
-                context.beginPath()
-                context.strokeStyle = 'black'
-                context.rect(x + (off / 2), 
-                              y + (off / 2), 
-                              w - off,h - off)
-                context.stroke()
-            }
-        }
-    }
   };
 };
 
