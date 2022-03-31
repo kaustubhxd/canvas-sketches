288abe6f6ba44e72f36cd4468a254627e65f1a32
itskaustubh <kaustubhpb@gmail.com>
2022-04-01 00:46:20 +0530

    draw sketch-02

--- sketch-02.js
+++ sketch-02.js
@@ -1,44 +1,45 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
 };
 
-const degToRad = (deg) => deg / 180 * Math.PI
+const degToRad = (deg) => (deg / 180) * Math.PI;
 
 const sketch = () => {
-  return ({ context : ctx, width, height }) => {
-    ctx.fillStyle = 'white';
+  return ({ context: ctx, width, height }) => {
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, width, height);
 
-    ctx.fillStyle = 'black'
-    x = width * 0.5
-    y = height * 0.5
-    w = width * 0.01
-    h = height * 0.1
+    ctx.fillStyle = "black";
+    x = width * 0.5;
+    y = height * 0.5;
+    w = width * 0.01;
+    h = height * 0.1;
 
-    const count = 12
-    const slice = 360 / 12
+    const count = 12;
+    const slice = 360 / 12;
 
-    
-    
-    for(let i=0;i<count;i++){
-      x = width * Math.random()
-      y = height * Math.random()
-      w = width * Math.random() * (0.04 - 0.01) + 0.01
-      h = height * Math.random()* (0.4 - 0.1) + 0.1
-      for(let j=0;j<count;j++){
-        ctx.save()
-        ctx.beginPath()
-        ctx.translate(x,y)
-        ctx.rotate(degToRad(slice * j))
-        ctx.rect(-(w * 0.5),-(h * 0.5),w,h)
-        ctx.fill()
-        ctx.restore()
+    ctx.font = "50px Georgia";
+    ctx.fillStyle = "red";
+    ctx.fillText("Work in progress", 0, 50);
+
+    for (let i = 0; i < count; i++) {
+      x = width * Math.random();
+      y = height * Math.random();
+      w = width * Math.random() * (0.04 - 0.01) + 0.01;
+      h = height * Math.random() * (0.4 - 0.1) + 0.1;
+      for (let j = 0; j < count; j++) {
+        ctx.save();
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.translate(x, y);
+        ctx.rotate(degToRad(slice * j));
+        ctx.rect(-(w * 0.5), -(h * 0.5), w, h);
+        ctx.fill();
+        ctx.restore();
       }
-    }  
-
-
+    }
   };
 };
 
