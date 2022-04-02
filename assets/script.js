801f88c47f34a5897bd192b4248fbd563a9d0ca9
itskaustubh <kaustubhpb@gmail.com>
2022-04-02 22:57:43 +0530

    landing page added

--- assets/script.js
+++ assets/script.js
@@ -20,40 +20,43 @@ function mapRange(value, inputMin, inputMax, outputMin, outputMax, clamp) {
   }
 }
 
-const worldMargin = 20;
-const mouseTrackMargin = 50;
+const mouseAimMarginX = 250;
+const mouseAimMarginY = 200;
 
-const handleMouseMove = (e) => {
-  const { clientX, clientY } = e;
+let currentX = 0;
+let currentY = 0;
+let aimX = 0;
+let aimY = 0;
+
+const handleMouseMove = (event) => {
+  const { clientX: mouseX, clientY: mouseY, pageX, pageY } = event;
   const { innerWidth: winWidth, innerHeight: winHeight } = window;
   const { clientWidth: worldWidth, clientHeight: worldHeight } = world;
 
-  console.log({ clientX, clientY, winWidth, winHeight, worldWidth, worldHeight });
-  //   const clientWidthMap = (clientX * winWidth) / worldWidth;
-  //   const clientHeightMap = (clientY * winHeight) / worldHeight;
+  //   console.log({ pageX, pageY, mouseX, mouseY, winWidth, winHeight, worldWidth, worldHeight });
 
-  const clientWidthMap = mapRange(
-    clientX,
-    worldMargin,
-    winWidth,
-    -worldMargin,
-    worldWidth - (winWidth - worldMargin)
-  );
-  const clientHeightMap = mapRange(
-    clientY,
-    worldMargin,
-    winHeight,
-    -worldMargin,
-    worldHeight - winHeight - worldMargin
-  );
+  const panWidth = worldWidth - winWidth;
+  const panHeight = worldHeight - winHeight;
 
-  console.log({ clientWidthMap, clientHeightMap });
+  const normalX = (mouseX - mouseAimMarginX) / (winWidth - 2 * mouseAimMarginX);
+  const normalY = (mouseY - mouseAimMarginY) / (winHeight - 2 * mouseAimMarginY);
 
-  // world.style.left = (clientWidthMap < worldMargin ? worldMargin : -clientWidthMap) + "px";
-  // world.style.top = (clientHeightMap < worldMargin ? worldMargin : -clientHeightMap) + "px";
+  aimX = -1 * (panWidth * normalX);
+  aimY = -1 * (panHeight * normalY);
 
-  world.style.left = -clientWidthMap + "px";
-  world.style.top = -clientHeightMap + "px";
+  //   console.log({ normalX, normalY });
 };
+
+const animate = () => {
+  currentX += (aimX - currentX) * 0.05;
+  currentY += (aimY - currentY) * 0.05;
+
+  world.style.left = currentX + "px";
+  world.style.top = currentY + "px";
+
+  requestAnimationFrame(animate);
+};
+
+animate();
 
 document.addEventListener("mousemove", handleMouseMove);
