cae50201a195ccefcad225367233fa83f1cb72a2
itskaustubh <kaustubhpb@gmail.com>
2022-05-30 18:44:39 +0000

    Deploying to gh-pages from @ itskaustubh/canvas-sketches@163c284b7d8a9706c6478a86394e4703d651df4a 🚀

--- sketch-07/index.js
+++ sketch-07/index.js
@@ -9263,10 +9263,15 @@
 // SKETCH 07: Squares in a circle rotating and revolving
 const canvasSketch = require("canvas-sketch");
 const Tweakpane = require("tweakpane");
+const { autoAdjustCanvas } = require("./utils/autoAdjustCanvas");
+
+console.log(autoAdjustCanvas);
 
 // html stuff
 document.title = document.URL.split("/").at(-2);
 document.body.style.backgroundColor = "aliceblue";
+document.body.style.overflowX = "hidden";
+document.body.style.overflowY = "hidden";
 
 const settings = {
   dimensions: [1080, 1080],
@@ -9360,10 +9365,10 @@ class Square {
   }
 }
 
-const createTweakPane = () => {
+const createTweakPane = ({ canvas }) => {
   const pane = new Tweakpane.Pane();
   const paneFolder = pane.addFolder({ title: "Tweakpane" });
-  // paneFolder.expanded = false;
+  paneFolder.expanded = false;
   paneFolder.addInput(params, "bgColor");
   paneFolder.addInput(params, "mainColor");
   paneFolder.addInput(params, "numberOfShapes", { min: 1, max: 100, step: 1, label: "No. of shapes" });
@@ -9375,6 +9380,9 @@ const createTweakPane = () => {
   paneFolder.addInput(params, "scaleMin", { min: 0, max: 1.5, step: 0.1 });
   paneFolder.addInput(params, "scaleMax", { min: 2, max: 3.5, step: 0.1 });
 
+  const panelNode = pane.containerElem_;
+  autoAdjustCanvas(canvas, panelNode, paneFolder.expanded);
+
   pane.on("change", (ev) => {
     console.log(ev);
     resetSketch();
@@ -9382,16 +9390,130 @@ const createTweakPane = () => {
   document.querySelector(".tp-dfwv").style.width = "280px";
 };
 
-canvasSketch(sketch, settings);
-createTweakPane();
+canvasSketch(sketch, settings).then(({ props }) => {
+  const { canvas } = props;
+  createTweakPane({ canvas });
+});
 
-},{"canvas-sketch":1,"tweakpane":2}],4:[function(require,module,exports){
+},{"./utils/autoAdjustCanvas":4,"canvas-sketch":1,"tweakpane":2}],4:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.autoAdjustCanvas = void 0;
+// Auto-adjusts canvas when tweakpane is open
+let canvasNode, isPanelExpanded;
+let originalSize = {
+  cSide: 0,
+  cMarginTop: 0,
+  cMarginLeft: 0
+};
+const resizeObserver = new ResizeObserver(entries => {
+  if (!isPanelExpanded) return;
+
+  for (let entry of entries) {
+    if (entry.contentRect) {
+      // check if the canvas and panel clash on the X axis
+      const panelMarginLeft = entry.target.offsetLeft;
+      const canvasMarginLeft = canvasNode.offsetLeft;
+      const canvasWidth = parseInt(canvasNode.style.width);
+      const isCanvasPanelClashingXAxis = canvasMarginLeft + canvasWidth > panelMarginLeft;
+      if (!isCanvasPanelClashingXAxis) return; // check which axis the canvas should translate to. if X-axis, exit resizeObserver
+
+      if (panelMarginLeft - canvasWidth > 0) {
+        console.log("translateLeft!");
+        canvasNode.style.transition = "all 0.3s ease-in-out";
+        canvasNode.style.marginLeft = originalSize.cMarginLeft + "px";
+        window.requestAnimationFrame(() => {
+          canvasNode.style.marginLeft = canvasMarginLeft - (canvasMarginLeft + canvasWidth - panelMarginLeft + 20) + "px"; // canvasNode.style.width = originalSize.cSide + "px";
+          // canvasNode.style.height = originalSize.cSide + "px";
+
+          canvasNode.addEventListener("transitionend", () => {
+            console.log("Transition ended");
+            canvasNode.style.transition = "none"; // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
+          });
+        });
+        return;
+      }
+
+      const panelHeight = +entry.contentRect.height;
+      const panelWidth = +entry.contentRect.width;
+      const panelMarginTop = entry.target.offsetTop;
+      const panelTotalHeightFromTop = panelHeight + panelMarginTop;
+      const canvasHeight = parseInt(canvasNode.style.height);
+      const canvasMarginTop = canvasNode.offsetTop;
+      const canvasTotalHeightFromTop = canvasHeight + canvasMarginTop;
+      const canvasPanelMarginTop = 8;
+      const canvasPanelMarginBottom = 20;
+
+      if (panelTotalHeightFromTop > canvasMarginTop) {
+        const canvasPanelOverlapHeight = panelTotalHeightFromTop - canvasMarginTop;
+        const updatedCanvasMarginTop = canvasMarginTop + canvasPanelOverlapHeight + canvasPanelMarginTop + "px";
+        console.log("panelclash", canvasPanelOverlapHeight, canvasMarginTop, updatedCanvasMarginTop);
+        canvasNode.style.marginTop = updatedCanvasMarginTop;
+      } // check if canvas overflows
+
+
+      const isCanvasPanelClashingYAxis = canvasMarginTop + canvasHeight > window.innerHeight;
+      console.log("leftclash", isCanvasPanelClashingXAxis);
+
+      if (isCanvasPanelClashingYAxis) {
+        const shrinkPx = canvasMarginTop + canvasHeight - window.innerHeight;
+        canvasNode.style.width = canvasWidth - (shrinkPx + canvasPanelMarginBottom) + "px";
+        canvasNode.style.height = canvasHeight - (shrinkPx + canvasPanelMarginBottom) + "px";
+      }
+
+      console.log("paneldown", {
+        panelTotalHeightFromTop,
+        canvasMarginTop
+      });
+      console.log(canvasNode, canvasNode.style); // const updatedCanvasMarginTop = canvasHeight + panelHeight + "px";
+      // console.log({ canvasHeight, canvasMarginTop, panelHeight, updatedCanvasMarginTop });
+    }
+  }
+});
+/**
+ * @param {HTMLElement} panelNode
+ * @param {CanvasRenderingContext2D} canvas canvas context
+ */
+
+const autoAdjustCanvas = (canvas, panelNode, isPanelOpen) => {
+  console.log(canvas, panelNode);
+  canvasNode = canvas;
+  resizeObserver.observe(panelNode);
+  originalSize = {
+    cSide: parseInt(canvasNode.style.width),
+    cMarginTop: canvasNode.offsetTop,
+    cMarginLeft: canvasNode.offsetLeft
+  };
+  panelNode.addEventListener("click", () => {
+    isPanelOpen = !isPanelOpen;
+    isPanelExpanded = isPanelOpen;
+    console.log("clicked!", isPanelOpen);
+
+    if (!isPanelOpen) {
+      canvasNode.style.transition = "all 0.3s ease-in-out 0s";
+      canvasNode.style.marginTop = originalSize.cMarginTop + "px";
+      canvasNode.style.width = originalSize.cSide + "px";
+      canvasNode.style.height = originalSize.cSide + "px";
+      canvasNode.addEventListener("transitionend", () => {
+        console.log("Transition ended");
+        canvasNode.style.transition = "none"; // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
+      });
+    }
+  });
+};
+
+exports.autoAdjustCanvas = autoAdjustCanvas;
+
+},{}],5:[function(require,module,exports){
 (function (global){(function (){
 
 global.CANVAS_SKETCH_DEFAULT_STORAGE_KEY = window.location.href;
 
 }).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 
-},{}]},{},[3,4])
+},{}]},{},[3,5])
 
 //# sourceMappingURL=index.js.map
