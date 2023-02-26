da90cf202a6bcb17dfd72e9754cb67e7776d71e0
kaustubhxd <kaustubhpb@gmail.com>
2023-02-26 06:03:41 +0000

    Deploying to gh-pages from @ kaustubhxd/canvas-sketches@830f5f943037a96ee0d2f25b0cf2fa349e64a2a8 🚀

--- sketch-07/index.js
+++ sketch-07/index.js
@@ -9403,6 +9403,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.autoAdjustCanvas = void 0;
 // Auto-adjusts canvas when tweakpane is open
+
 let canvasNode, isPanelExpanded;
 let originalSize = {
   cSide: 0,
@@ -9411,7 +9412,6 @@ let originalSize = {
 };
 const resizeObserver = new ResizeObserver(entries => {
   if (!isPanelExpanded) return;
-
   for (let entry of entries) {
     if (entry.contentRect) {
       // check if the canvas and panel clash on the X axis
@@ -9419,24 +9419,26 @@ const resizeObserver = new ResizeObserver(entries => {
       const canvasMarginLeft = canvasNode.offsetLeft;
       const canvasWidth = parseInt(canvasNode.style.width);
       const isCanvasPanelClashingXAxis = canvasMarginLeft + canvasWidth > panelMarginLeft;
-      if (!isCanvasPanelClashingXAxis) return; // check which axis the canvas should translate to. if X-axis, exit resizeObserver
+      if (!isCanvasPanelClashingXAxis) return;
 
+      // check which axis the canvas should translate to. if X-axis, exit resizeObserver
       if (panelMarginLeft - canvasWidth > 0) {
         console.log("translateLeft!");
         canvasNode.style.transition = "all 0.3s ease-in-out";
         canvasNode.style.marginLeft = originalSize.cMarginLeft + "px";
         window.requestAnimationFrame(() => {
-          canvasNode.style.marginLeft = canvasMarginLeft - (canvasMarginLeft + canvasWidth - panelMarginLeft + 20) + "px"; // canvasNode.style.width = originalSize.cSide + "px";
+          canvasNode.style.marginLeft = canvasMarginLeft - (canvasMarginLeft + canvasWidth - panelMarginLeft + 20) + "px";
+          // canvasNode.style.width = originalSize.cSide + "px";
           // canvasNode.style.height = originalSize.cSide + "px";
-
           canvasNode.addEventListener("transitionend", () => {
             console.log("Transition ended");
-            canvasNode.style.transition = "none"; // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
+            canvasNode.style.transition = "none";
+            // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
           });
         });
+
         return;
       }
-
       const panelHeight = +entry.contentRect.height;
       const panelWidth = +entry.contentRect.width;
       const panelMarginTop = entry.target.offsetTop;
@@ -9446,38 +9448,37 @@ const resizeObserver = new ResizeObserver(entries => {
       const canvasTotalHeightFromTop = canvasHeight + canvasMarginTop;
       const canvasPanelMarginTop = 8;
       const canvasPanelMarginBottom = 20;
-
       if (panelTotalHeightFromTop > canvasMarginTop) {
         const canvasPanelOverlapHeight = panelTotalHeightFromTop - canvasMarginTop;
         const updatedCanvasMarginTop = canvasMarginTop + canvasPanelOverlapHeight + canvasPanelMarginTop + "px";
         console.log("panelclash", canvasPanelOverlapHeight, canvasMarginTop, updatedCanvasMarginTop);
         canvasNode.style.marginTop = updatedCanvasMarginTop;
-      } // check if canvas overflows
+      }
 
-
+      // check if canvas overflows
       const isCanvasPanelClashingYAxis = canvasMarginTop + canvasHeight > window.innerHeight;
       console.log("leftclash", isCanvasPanelClashingXAxis);
-
       if (isCanvasPanelClashingYAxis) {
         const shrinkPx = canvasMarginTop + canvasHeight - window.innerHeight;
         canvasNode.style.width = canvasWidth - (shrinkPx + canvasPanelMarginBottom) + "px";
         canvasNode.style.height = canvasHeight - (shrinkPx + canvasPanelMarginBottom) + "px";
       }
-
       console.log("paneldown", {
         panelTotalHeightFromTop,
         canvasMarginTop
       });
-      console.log(canvasNode, canvasNode.style); // const updatedCanvasMarginTop = canvasHeight + panelHeight + "px";
+      console.log(canvasNode, canvasNode.style);
+
+      // const updatedCanvasMarginTop = canvasHeight + panelHeight + "px";
       // console.log({ canvasHeight, canvasMarginTop, panelHeight, updatedCanvasMarginTop });
     }
   }
 });
+
 /**
  * @param {HTMLElement} panelNode
  * @param {CanvasRenderingContext2D} canvas canvas context
  */
-
 const autoAdjustCanvas = (canvas, panelNode, isPanelOpen) => {
   console.log(canvas, panelNode);
   canvasNode = canvas;
@@ -9491,7 +9492,6 @@ const autoAdjustCanvas = (canvas, panelNode, isPanelOpen) => {
     isPanelOpen = !isPanelOpen;
     isPanelExpanded = isPanelOpen;
     console.log("clicked!", isPanelOpen);
-
     if (!isPanelOpen) {
       canvasNode.style.transition = "all 0.3s ease-in-out 0s";
       canvasNode.style.marginTop = originalSize.cMarginTop + "px";
@@ -9499,12 +9499,12 @@ const autoAdjustCanvas = (canvas, panelNode, isPanelOpen) => {
       canvasNode.style.height = originalSize.cSide + "px";
       canvasNode.addEventListener("transitionend", () => {
         console.log("Transition ended");
-        canvasNode.style.transition = "none"; // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
+        canvasNode.style.transition = "none";
+        // canvasNode.removeEventListener("transitionend", handleMouseDown, true);
       });
     }
   });
 };
-
 exports.autoAdjustCanvas = autoAdjustCanvas;
 
 },{}],5:[function(require,module,exports){
